898776d13042867055aff7e630b6cab5c4cd6889
Harastar1001 <total.dev0987@gmail.com>
2025-01-15 21:29:46 +0000

    feat: subscribing to logs

--- src/common/utils.rs
+++ src/common/utils.rs
@@ -5,6 +5,15 @@ use solana_sdk::signature;
 use std::error::Error;
 use fern::colors::{ Color, ColoredLevelConfig };
 use log::LevelFilter;
+use solana_sdk::pubkey::Pubkey;
+// use solana_client::websocket::{SolanaWebSocket, WebSocketConfig};
+use solana_client::pubsub_client::PubsubClient;
+use solana_client::rpc_config::{
+  RpcTransactionLogsFilter,
+  RpcTransactionLogsConfig,
+  RpcAccountInfoConfig,
+};
+use solana_sdk::commitment_config::{ CommitmentConfig, CommitmentLevel };
 
 use crate::common::constants::PROJECT_NAME;
 
@@ -93,27 +102,81 @@ pub fn parse_transaction(transaction: Option<&Value>) -> Map<String, Value> {
   let mut transaction_info = Map::new();
 
   if let Some(transaction) = transaction {
-    println!("transaction value exist");
-      if let Some(transaction_map) = transaction.get("transaction").and_then(Value::as_object) {
-          let signatures = transaction_map.get("signatures").cloned().unwrap_or_else(|| Value::Array(vec![]));
-          transaction_info.insert("signatures".to_string(), signatures);
+    if let Some(transaction_map) = transaction.get("transaction").and_then(Value::as_object) {
+      let signatures = transaction_map
+        .get("signatures")
+        .cloned()
+        .unwrap_or_else(|| Value::Array(vec![]));
+      transaction_info.insert("signatures".to_string(), signatures);
 
-          let message = transaction_map.get("message").and_then(Value::as_object).unwrap_or(&Map::new()).clone();
-          let instructions = message.get("instructions").cloned().unwrap_or_else(|| Value::Array(vec![]));
-          transaction_info.insert("instructions".to_string(), instructions);
+      let message = transaction_map
+        .get("message")
+        .and_then(Value::as_object)
+        .unwrap_or(&Map::new())
+        .clone();
+      let instructions = message
+        .get("instructions")
+        .cloned()
+        .unwrap_or_else(|| Value::Array(vec![]));
+      transaction_info.insert("instructions".to_string(), instructions);
 
-          if let Some(meta) = transaction.get("meta").and_then(Value::as_object) {
-              let post_balances = meta.get("postBalances").cloned().unwrap_or_else(|| Value::Array(vec![]));
-              transaction_info.insert("postBalances".to_string(), post_balances);
+      if let Some(meta) = transaction.get("meta").and_then(Value::as_object) {
+        let post_balances = meta
+          .get("postBalances")
+          .cloned()
+          .unwrap_or_else(|| Value::Array(vec![]));
+        transaction_info.insert("postBalances".to_string(), post_balances);
 
-              let pre_balances = meta.get("preBalances").cloned().unwrap_or_else(|| Value::Array(vec![]));
-              transaction_info.insert("preBalances".to_string(), pre_balances);
+        let pre_balances = meta
+          .get("preBalances")
+          .cloned()
+          .unwrap_or_else(|| Value::Array(vec![]));
+        transaction_info.insert("preBalances".to_string(), pre_balances);
 
-              let status = meta.get("status").cloned().unwrap_or_else(|| Value::Object(Map::new()));
-              transaction_info.insert("status".to_string(), status);
-          }
+        let status = meta
+          .get("status")
+          .cloned()
+          .unwrap_or_else(|| Value::Object(Map::new()));
+        transaction_info.insert("status".to_string(), status);
       }
+    }
   }
 
   transaction_info
+}
+
+pub async fn logs_subscribe(
+  ws_url: &str,
+  account_address: &str,
+  commitment: &str
+) -> Result<(), Box<dyn std::error::Error>> {
+  // ) -> () {
+    const SOLANA_RPC_URL_WS: &str = "wss://api.mainnet-beta.solana.com/";
+  let filter = RpcTransactionLogsFilter::Mentions(
+    vec![String::from("CPMMoo8L3F4NbTegBCKVNunggL7H1ZpdTHKxQB5qKP1C")]
+  );
+  let config = RpcTransactionLogsConfig {
+    commitment: Some(CommitmentConfig::processed()),
+  };
+
+  let subscription_result = PubsubClient::logs_subscribe(&SOLANA_RPC_URL_WS, filter, config);
+  match subscription_result {
+    Ok((tx_log, rx_log)) => {
+      loop {
+        match rx_log.recv() {
+            Ok(response) => {
+              println!("{:?}\n", response.value.logs);
+            }
+            Err(e) => {
+              println!("Log Subscription Error: {:?}", e);
+            }
+        }
+      }
+    }
+    Err(e) => {
+      println!("Failed to subscribe to log: {:?}", e);
+    }
+  }
+
+  Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 mod common;
 
-use common::utils::{fetch_blockchain_data, get_confirmed_signatures_for_address, get_transaction_details, parse_transaction};
+use common::utils::{
+  fetch_blockchain_data,
+  get_confirmed_signatures_for_address,
+  get_transaction_details,
+  parse_transaction,
+  logs_subscribe
+};
 
 #[tokio::main]
 async fn main() {
-    let signature = String::from("2xFiis5uErY2uzGaMs11wiPfkWBHEaCKpmuJpXhTonxjU4oT2BewpZ9WVXfSswVEyNsj2vif9Zo5YtYt2GSK1aZd");
-    fetch_blockchain_data();
-    // match get_confirmed_signatures_for_address(10).await {
-    //     Ok(response) => println!("Response: {:?}", response),
-    //     Err(e) => eprintln!("Error: {}", e),
-    // }
-    match get_transaction_details(&signature).await {
-        Ok(response) => println!("transation details: {:?}", parse_transaction(Some(&response))),
-        Err(e) => eprintln!("Error: {}", e),
-    }
+//   let signature = String::from(
+//     "2xFiis5uErY2uzGaMs11wiPfkWBHEaCKpmuJpXhTonxjU4oT2BewpZ9WVXfSswVEyNsj2vif9Zo5YtYt2GSK1aZd"
+//   );
+//   fetch_blockchain_data();
+  // match get_confirmed_signatures_for_address(10).await {
+  //     Ok(response) => println!("Response: {:?}", response),
+  //     Err(e) => eprintln!("Error: {}", e),
+  // }
+//   match get_transaction_details(&signature).await {
+//     Ok(response) => println!("transation details: {:?}", parse_transaction(response.get("result"))),
+//     Err(e) => eprintln!("Error: {}", e),
+//   }
+  let ws_url = "wss://api.mainnet-beta.solana.com/ws";
+//   let ws_url = "https://api.mainnet-beta.solana.com";
+  let account_address = "So11111111111111111111111111111111111111112";
+  let commitment = "confirmed"; 
+  logs_subscribe(ws_url, account_address, commitment).await;
 }
